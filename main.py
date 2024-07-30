9fd537d2bf983b897aa22344df1d54364dc89fea
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-07-30 20:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import pandas as pd
 
-
-def main():
-    
+def main():  
     df = pd.read_excel('Google Launchpad.xlsx', sheet_name='Sheet1')
     df = df.sample(frac=1).reset_index(drop=True)
-    df['Your contact number'] = df['Your contact number'].astype(str)
-    
+    df['Contact Number Column'] = df['Contact Number Column'].astype(str)
     
     no_of_groups = int(input('Enter the number of groups: '))
-
-    
+ 
     male_df = df[df['Gender'] == 'Male']
     female_df = df[df['Gender'] == 'Female']
-
     
     males_per_group = len(male_df) // no_of_groups
     females_per_group = len(female_df) // no_of_groups
 
-    
     remaining_male = len(male_df) % no_of_groups
     remaining_female = len(female_df) % no_of_groups
 
-    
     groups = []
     male_start_index = female_start_index = 0
     for i in range(no_of_groups):
@@ -34,7 +27,6 @@ def main():
         if i < remaining_female:
             female_end_index += 1
 
-        
         group = pd.concat([male_df.iloc[male_start_index:male_end_index], 
                            female_df.iloc[female_start_index:female_end_index]]) 
         groups.append(group)
@@ -42,11 +34,9 @@ def main():
         male_start_index = male_end_index
         female_start_index = female_end_index
     
-    
     with pd.ExcelWriter('output.xlsx', engine='xlsxwriter') as writer:
         for indx, group in enumerate(groups):
             group.to_excel(writer, sheet_name=f"Group {indx + 1}", index=False)
-
 
 if __name__ == "__main__":
     main()
